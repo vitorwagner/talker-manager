0d0c413d50c55ae70688da2265a72840c9348876
Vitor Wagner <vitormwagner@gmail.com>
2023-03-14 17:27:04 -0300

    Refactor: :recycle: refactored writeFile.js

--- src/utils/writeFile.js
+++ src/utils/writeFile.js
@@ -1,11 +1,8 @@
 const path = require('path');
 const { writeFile } = require('fs').promises;
-const readTalkers = require('./readFile');
 
-async function writeTalker(talker) {
-  const talkers = await readTalkers();
-  talkers.push(talker);
-  await writeFile(path.join(__dirname, '..', 'talker.json'), JSON.stringify(talker));
+async function writeTalker(talkers) {
+  await writeFile(path.join(__dirname, '..', 'talker.json'), JSON.stringify(talkers));
 }
 
 module.exports = writeTalker;
